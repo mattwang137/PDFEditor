b90da68f134cd5b6aaebba4205fcd22e9c59b2a6
Matt Wang <mattwang137@exentric.com.tw>
2020-10-26 15:06:31 +0800

    add ReplaceTextOfPdf by Spire.Pdf and SpireHelper.cs

--- PDFEditor/Program.cs
+++ PDFEditor/Program.cs
@@ -1,6 +1,4 @@
-﻿using iTextSharp.text.pdf;
-using iTextSharp.text.pdf.parser;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
@@ -8,6 +6,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Path = System.IO.Path;
+using SpirePdf = Spire.Pdf;
+using iTextSharpPdf = iTextSharp.text.pdf;
+using Spire.Pdf;
+using Spire.Pdf.General.Find;
+using Spire.Pdf.Graphics;
+using System.Drawing;
+using Org.BouncyCastle.Asn1.Cmp;
 
 namespace PDFEditor
 {
@@ -15,30 +20,73 @@ namespace PDFEditor
     {
         static void Main(string[] args)
         {
-            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "sample.pdf");
+            /*
+             * 為了刪除 Evaluation Warning : The document was created with Spire.PDF for .NET.字串
+             * 第一次運行的時候運行SpireHelper.cs，修改Spire.License.dll中的密鑰
+             */
+            //PDFEditor.SpireHelper.ActivateMemoryPatching();
 
-            Console.WriteLine(ExtractTextFormPdf(filePath));
+            string pdfFileName = "sample.pdf";
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, pdfFileName);
+
+            //Console.WriteLine(ExtractTextFormPdf(filePath));
+            ReplaceTextOfPdf(pdfFileName);
+
             Console.ReadKey();
         }
 
-        
 
-        public static string ExtractTextFormPdf(string pdfFileName)
+        // using Spire.Pdf 達成替換字功能
+        public static void ReplaceTextOfPdf(string pdfFileName)
+        {
+            string targetText = "申請";
+            string replaceText = "替代";
+
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, pdfFileName);
+            SpirePdf.PdfDocument file = new SpirePdf.PdfDocument();
+
+            file.LoadFromFile(filePath);
+
+            PdfBrush brush = new PdfSolidBrush(Color.Black);
+            // 英文 font
+            PdfTrueTypeFont font = new PdfTrueTypeFont(new Font("Arial", 12f, FontStyle.Regular));
+
+            // 繁體中文 font
+            PdfCjkStandardFont font1 = new PdfCjkStandardFont(PdfCjkFontFamily.MonotypeSungLight, 12f);
+
+            //字的正方形框
+            RectangleF rec;
+            foreach (PdfPageBase eachPage in file.Pages)
+            {
+                PdfTextFindCollection collection = eachPage.FindText(targetText, TextFindParameter.IgnoreCase);
+
+                foreach (PdfTextFind find in collection.Finds)
+                {
+                    rec = find.Bounds;
+                    eachPage.Canvas.DrawRectangle(PdfBrushes.Yellow, rec);
+                    eachPage.Canvas.DrawString(replaceText, font1, brush, rec);
+                }
+            }
+
+            file.SaveToFile(pdfFileName.Replace(".", "_fixed."));
+
+        }
+
+        public static string ExtractTextFormPdf(string pdfPath)
         {
 
             StringBuilder text = new StringBuilder();
             try
             {
-                PdfReader pdfReader = new PdfReader(pdfFileName);
+                iTextSharpPdf.PdfReader pdfReader = new iTextSharpPdf.PdfReader(pdfPath);
                 int pageNumber = pdfReader.NumberOfPages;
-                for(int i=0 ; i<pageNumber ; i += 1)
+                for (int i = 0; i < pageNumber; i += 1)
                 {
-                    text.Append(iTextSharp.text.pdf.parser.PdfTextExtractor.GetTextFromPage(pdfReader, i + 1));
-                    int a = 1;
+                    text.Append(iTextSharpPdf.parser.PdfTextExtractor.GetTextFromPage(pdfReader, i + 1));
                 }
                 pdfReader.Close();
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
             }
@@ -49,4 +97,7 @@ namespace PDFEditor
 
 
     }
+
+
+    
 }
