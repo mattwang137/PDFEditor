15b52cdd0c0223a79f165da2a92dc2dcc0d9327d
Matt Wang <mattwang137@exentric.com.tw>
2020-10-26 11:29:30 +0800

    add ExtractTextFormPdf

--- PDFEditor/Program.cs
+++ PDFEditor/Program.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Path = System.IO.Path;
 
 namespace PDFEditor
 {
@@ -10,6 +15,38 @@ namespace PDFEditor
     {
         static void Main(string[] args)
         {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "sample.pdf");
+
+            Console.WriteLine(ExtractTextFormPdf(filePath));
+            Console.ReadKey();
         }
+
+        
+
+        public static string ExtractTextFormPdf(string pdfFileName)
+        {
+
+            StringBuilder text = new StringBuilder();
+            try
+            {
+                PdfReader pdfReader = new PdfReader(pdfFileName);
+                int pageNumber = pdfReader.NumberOfPages;
+                for(int i=0 ; i<pageNumber ; i += 1)
+                {
+                    text.Append(iTextSharp.text.pdf.parser.PdfTextExtractor.GetTextFromPage(pdfReader, i + 1));
+                    int a = 1;
+                }
+                pdfReader.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            return text.ToString();
+
+        }
+
+
     }
 }
